869a01e273cd01430ed4f97dba462e66a50c2a8c
Anshul Gupta <aguptapro@gmail.com>
2022-10-01 18:09:47 -0700

    Update HiveJDBC.java

--- HiveJDBC.java
+++ HiveJDBC.java
@@ -5,24 +5,25 @@ import java.sql.Statement;
 
 public class HiveJDBC {
 
-  // Parameters
-  static String CONNECTION_URL = "<HIVE_JDBC_URL";
-  static String QUERY = "<HIVE_QUERY>";
+	// Parameters
+	static String CONNECTION_URL = "<HIVE_JDBC_URL";
+	static String QUERY = "<HIVE_QUERY>";
 
 	/**
 	 * Main method
+	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
-      // establish connection
+			// establish connection
 			Connection connection = null;
 			connection = DriverManager.getConnection(CONNECTION_URL);
 
 			// execute query
 			execQry(connection);
 
-		  // close connection
+			// close connection
 			connection.close();
 
 		} catch (Exception e) {
@@ -32,20 +33,21 @@ public class HiveJDBC {
 
 	/**
 	 * Execute Query
+	 *
 	 * @param connection
 	 */
 	public static void execQry(Connection connection) {
 		try {
-      // execute query
-		  Statement stmt = connection.createStatement();
-		  ResultSet rs = stmt.executeQuery(QUERY);
+			// execute query
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(QUERY);
 
-		  // print output
-		  System.out.println("Hive test ------- ");
-		  while (rs.next()) {
-        // print 1st column values
-        System.out.println(rs.getString(1));
-		  }
+			// print output
+			System.out.println("Hive test ------- ");
+			while (rs.next()) {
+				// print 1st column values
+				System.out.println(rs.getString(1));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -53,14 +55,15 @@ public class HiveJDBC {
 
 	/**
 	 * Wait method to test queries after intervals for troubleshooting
+	 *
 	 * @param ms
 	 */
 	public static void wait(int ms) {
-    try {
-      Thread.sleep(ms);
-    } catch (InterruptedException e) {
-      System.err.format("IOException: %s%n", e);
-    }
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			System.err.format("IOException: %s%n", e);
+		}
 	}
 
 }
